74df974da253ad440093c7f1c76d2936b1337dfe
csababalint91 <40868013+csababalint91@users.noreply.github.com>
2018-10-23 19:03:50 +0100

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var button = document.getElementById("btn");
-  
-  button.addEventListener("click", function() {
-    var box = document.getElementById("box");
-  	box.classList.add("box");
-  });
-});
+function myFunction() {
+   var element = document.getElementById("about");
+   element.classList.toggle("mystyle");
 
+}
